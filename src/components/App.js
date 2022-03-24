3ff1851ed9b243876f6f394c8f2a57fba8861c92
anw0514 <anw0514@gmail.com>
2022-03-24 13:18:31 -0400

    framework

---
+++ src/components/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import DetailedUsers from './DetailedUsers';
+import UserList from './UserList';
+
+function App() {
+  return (
+    <div className="App">
+      <UserList/>
+      <DetailedUsers/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/DetailedUsers.js
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function DetailedUsers() {
+  return (
+    <div>DetailedUsers</div>
+  )
+}
